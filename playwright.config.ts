c783aab6ec239c73a38e10d8ea2dd098a5529ea8
ofir3232 <ofirc2005@gmail.com>
2026-03-17 09:54:21 +0200

    fix var name

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: process.env.Base_URL,
+    baseURL: process.env.BASE_URL,
 
     screenshot: 'only-on-failure', 
     video: 'retain-on-failure', 
